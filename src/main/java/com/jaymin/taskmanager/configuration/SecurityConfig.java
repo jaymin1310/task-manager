d8fcb1358c8ca39d288e7763fdbe185a7720a949
jaymin1310 <jvsolanki2005@gmail.com>
2026-03-09 22:05:01 +0530

    updated security config and customUserDetails

--- src/main/java/com/jaymin/taskmanager/configuration/SecurityConfig.java
+++ src/main/java/com/jaymin/taskmanager/configuration/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .sessionManagement(session->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
--- src/main/java/com/jaymin/taskmanager/security/CustomUserDetails.java
+++ src/main/java/com/jaymin/taskmanager/security/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.jaymin.taskmanager.security;
 
 import com.jaymin.taskmanager.entity.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,8 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
-
-@Component
 public class CustomUserDetails implements UserDetails {
     private final User user;
     public CustomUserDetails(User user) {
@@ -25,7 +24,7 @@ public class CustomUserDetails implements UserDetails {
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE"+user.getRole().name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getRole().name()));
     }
     @Override
     public boolean isAccountNonExpired() {
